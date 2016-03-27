42697b4bd078dd16020e9b75d4e1827cab3a8920
Giles Thomas <giles@pythonanywhere.com>
2016-03-27 20:09:17 +0000

    First cut at a data-downloader, with all ishares and vanguard funds/ETFs I can find.

---
+++ history_downloader.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.4
+
+import csv
+import datetime
+import os
+import pandas_datareader.data as datareader
+import time
+
+start = datetime.datetime(2011, 1, 1)
+end = datetime.datetime(2015, 12, 31)
+
+for filename in ("ishares.csv", "vanguard.csv"):
+    print("Loading {}".format(filename))
+    with open(filename, "r") as f:
+        reader = csv.reader(f, delimiter=",", quotechar='"')
+        for row in reader:
+            ticker = row[0]
+
+            print("About to read data for {}".format(ticker))
+            for retry in range(2):
+                try:
+                    frame = datareader.DataReader(ticker, "yahoo", start, end)
+                    print("Done reading data for {}, storing".format(ticker))
+                    frame.to_csv(os.path.join("data", ticker + ".csv"))
+                    print("Done storing {}".format(ticker))
+                    break
+                except:
+                    print("Got an error loading {}, will pause and retry".format(ticker))
+                    time.sleep(5)
