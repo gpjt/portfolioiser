4202c2f704818f61425bf718a2f031ae216e64d9
Giles Thomas <giles@pythonanywhere.com>
2016-04-03 15:39:28 +0000

    Run with a larger population size, for 750 generations, starting with the toughest time period.

--- evolve.py
+++ evolve.py
@@ -9,11 +9,11 @@ import time
 from fitness_function import judge_fitness
 from portfolio import Portfolio
 
-POPULATION_SIZE = 5000
+POPULATION_SIZE = 20000
 PORTFOLIO_SIZE = 8
 GLOBAL_START_DATE = datetime(2012, 7, 17)
 GLOBAL_END_DATE = datetime(2016, 3, 15)
-GENERATIONS = 10
+GENERATIONS = 750
 
 
 def generate_random_portfolio():
@@ -68,73 +68,87 @@ print("Done generating, got {} portfolios".format(len(population)))
 
 
 start_dates = [
+    datetime(2012, 7, 17),
     datetime(2013, 1, 1),
+    datetime(2013, 6, 1),
+    datetime(2014, 1, 1),
     datetime(2014, 6, 1),
     datetime(2015, 1, 1),
-    datetime(2014, 1, 1),
-    datetime(2013, 6, 1),
-    datetime(2015, 3, 1),
+    datetime(2015, 3, 2),
 ]
-for start_date in start_dates:
+
+for generation in range(GENERATIONS):
+    if generation > 32 and generation < GENERATIONS - 1:
+        if generation % 5 == 0:
+            start_date = start_dates[-1]
+        else:
+            start_date = random.choice(start_dates)
+    else:
+        start_date = start_dates[-1]
     end_date = start_date + timedelta(days=365)
 
-    for generation in range(GENERATIONS):
-        print()
-        print("*" * 80)
-        print("Generation {}".format(generation))
-        print("*" * 80)
-        print()
-        print("Judging fitness over {} to {}".format(start_date, end_date))
-        start_time = time.time()
-        metrics_portfolios = []
-        for portfolio in population:
-            trace = portfolio.trace(market_data, start_date, end_date)
-            fitness, calmness, return_over_period = judge_fitness(trace.frame)
-            metrics_portfolios.append((fitness, calmness, return_over_period, portfolio))
-        metrics_portfolios = sorted(metrics_portfolios, key=lambda element: element[0])
-        print("Done collating fitness, took {}s".format(time.time() - start_time))
 
-        print("Finished generation {}. Stats (min / max / average)".format(generation))
-        fitnesses = [fitness for (fitness, _, _, _) in metrics_portfolios]
-        print("Fitness: {} / {} / {}".format(min(fitnesses), max(fitnesses), np.mean(fitnesses)))
-        calmnesses  = [calmness for (_, calmness, _, _) in metrics_portfolios]
-        print("Calmness: {} / {} / {}".format(min(calmnesses), max(calmnesses), np.mean(calmnesses)))
-        returns_over_period  = [return_over_period for (_, _, return_over_period, _) in metrics_portfolios]
-        print("Returns: {} / {} / {}".format(min(returns_over_period), max(returns_over_period), np.mean(returns_over_period)))
-        winner_fitness, winner_calmness, winner_returns_over_period, winner_porfolio = metrics_portfolios[-1]
-        print("Winner fitness {}".format(winner_fitness))
-        print("Winner calmness {}".format(winner_calmness))
-        print("Winner return {}".format(winner_returns_over_period))
-        print("Winner holdings {}".format(winner_porfolio.holdings))
+    print()
+    print("*" * 80)
+    print("Generation {}".format(generation))
+    print("*" * 80)
+    print()
+    print("Judging fitness over {} to {}".format(start_date, end_date))
+    start_time = time.time()
+    metrics_portfolios = []
+    for portfolio in population:
+        trace = portfolio.trace(market_data, start_date, end_date)
+        fitness, calmness, return_over_period = judge_fitness(trace.frame)
+        metrics_portfolios.append((fitness, calmness, return_over_period, portfolio))
+    metrics_portfolios = sorted(metrics_portfolios, key=lambda element: element[0])
+    print("Done collating fitness, took {}s".format(time.time() - start_time))
 
-        if generation < GENERATIONS - 1:
-            print("Culling...")
-            new_population = set()
-            # Kill weakest half plus any with zero rating
-            for rating, _, _, portfolio in metrics_portfolios[int(len(metrics_portfolios) / 2):]:
-                if rating != 0:
-                    new_population.add(portfolio)
-            print("{} survivors ({}%)".format(len(new_population), len(new_population) * 100 / len(population)))
+    print("Finished generation {}. Stats (min / max / average)".format(generation))
+    fitnesses = [fitness for (fitness, _, _, _) in metrics_portfolios]
+    print("Fitness: {} / {} / {}".format(min(fitnesses), max(fitnesses), np.mean(fitnesses)))
+    calmnesses  = [calmness for (_, calmness, _, _) in metrics_portfolios]
+    print("Calmness: {} / {} / {}".format(min(calmnesses), max(calmnesses), np.mean(calmnesses)))
+    returns_over_period  = [return_over_period for (_, _, return_over_period, _) in metrics_portfolios]
+    print("Returns: {} / {} / {}".format(min(returns_over_period), max(returns_over_period), np.mean(returns_over_period)))
+    winner_fitness, winner_calmness, winner_returns_over_period, winner_porfolio = metrics_portfolios[-1]
+    print("Winner fitness {}".format(winner_fitness))
+    print("Winner calmness {}".format(winner_calmness))
+    print("Winner return {}".format(winner_returns_over_period))
+    print("Winner holdings:")
+    for ticker, holding in winner_porfolio.holdings:
+        print("{}, {}".format(ticker, holding))
 
-            new_portfolio_count = len(population) - len(new_population)
-            print("Need {} new portfolios".format(new_portfolio_count))
+    if generation < GENERATIONS - 1:
+        print("Culling...")
+        new_population = set()
+        # Kill weakest half plus any with zero rating
+        for rating, _, _, portfolio in metrics_portfolios[int(len(metrics_portfolios) / 2):]:
+            if rating != 0:
+                new_population.add(portfolio)
+        print("{} survivors ({}%)".format(len(new_population), len(new_population) * 100 / len(population)))
+        if len(new_population) < 100:
+            print("BAILOUT extinction-level event")
+            raise Exception()
 
-            print("Creating 90% mutated portfolios weighting reproduction chances to highest-ranked")
-            reproducing_population = [portfolio for (_, _, _, portfolio) in metrics_portfolios]
-            reproduction_chance_unnormalised = range(len(reproducing_population))
-            normalisation_factor = sum(reproduction_chance_unnormalised)
-            reproduction_chance = [v / normalisation_factor for v in reproduction_chance_unnormalised]
-            for jj in range(int(new_portfolio_count * 0.9)):
-                new_population.add(mutate(np.random.choice(reproducing_population, None, p=reproduction_chance)))
-            print("New population is now {}".format(len(new_population)))
+        new_portfolio_count = len(population) - len(new_population)
+        print("Need {} new portfolios".format(new_portfolio_count))
 
-            to_fill = POPULATION_SIZE - len(new_population)
-            print("Generating {} new random portfolios".format(to_fill))
-            for jj in range(to_fill):
-                new_population.add(generate_random_portfolio())
-            print("Done generating new random portfolios")
+        print("Creating 90% mutated portfolios weighting reproduction chances to highest-ranked")
+        reproducing_population = [portfolio for (_, _, _, portfolio) in metrics_portfolios]
+        reproduction_chance_unnormalised = range(len(reproducing_population))
+        normalisation_factor = sum(reproduction_chance_unnormalised)
+        reproduction_chance = [v / normalisation_factor for v in reproduction_chance_unnormalised]
+        for jj in range(int(new_portfolio_count * 0.9)):
+            new_population.add(mutate(np.random.choice(reproducing_population, None, p=reproduction_chance)))
+        print("New population is now {}".format(len(new_population)))
 
-            population = new_population
+        to_fill = POPULATION_SIZE - len(new_population)
+        print("Generating {} new random portfolios".format(to_fill))
+        for jj in range(to_fill):
+            new_population.add(generate_random_portfolio())
+        print("Done generating new random portfolios")
+
+        population = new_population
 
 
 
